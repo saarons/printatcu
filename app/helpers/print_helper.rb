ff7314e1db77b24753094d6c622c3cef3322f578
Sam Aarons <samaarons@gmail.com>
2011-12-08 04:37:26 -0500

    Fix small bug

--- app/helpers/print_helper.rb
+++ app/helpers/print_helper.rb
@@ -1,2 +1,9 @@
 module PrintHelper
+  def buildings
+    $buildings.sort
+  end
+  
+  def default_printer
+    @print.printer.present? ? $printers[@print.building] : $printers[buildings.first]
+  end
 end
