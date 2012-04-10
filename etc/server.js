342ef70310ac63d75884c450326696df43a5041d
Sam Aarons <samaarons@gmail.com>
2012-04-09 22:55:47 -0400

    Only use XHR polling

--- etc/server.js
+++ etc/server.js
@@ -3,6 +3,11 @@ var client = redis.createClient();
 
 var io = require("socket.io").listen(parseInt(process.env.PORT));
 
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 client.on("message", function (channel, message) {
     io.sockets.emit(channel, message);
 });
