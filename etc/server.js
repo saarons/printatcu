3c67c4354b861dc5cc176d04ea8f85156a1109aa
Sam Aarons <samaarons@gmail.com>
2012-04-09 21:55:53 -0400

    Fix node server

--- etc/server.js
+++ etc/server.js
@@ -1,7 +1,10 @@
 var redis = require("redis");
 var client = redis.createClient();
 
-var io = require("socket.io").listen(parseInt(process.env.PORT));
+var server = http.createServer();
+server.listen(parseInt(process.env.PORT), "66.228.56.230");
+
+var io = require("socket.io").listen(server);
 
 client.on("message", function (channel, message) {
     io.sockets.emit(channel, message);
