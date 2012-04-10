cac0979722950757e53f7a7b3999dc9584ab17eb
Sam Aarons <samaarons@gmail.com>
2012-04-09 20:05:27 -0400

    Add redis hook

--- app/models/print_worker.rb
+++ app/models/print_worker.rb
@@ -4,7 +4,10 @@ class PrintWorker
   def self.perform(document_id)
     document = Document.find(document_id)
     ok = document.needs_conversion? ? document.convert : true
-    document.enqueue if ok
+    if ok
+      document.enqueue
+      $redis.publish("print", ActiveSupport::JSON.encode({building: document.print.building}))
+    end
     document.cleanup
   end
 end
