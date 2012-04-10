92fe1af771cdbf989a2fd4c0a0ff5e32d8d82b43
Sam Aarons <samaarons@gmail.com>
2012-04-09 20:33:20 -0400

    Add node server

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+node: node etc/server.js
 worker:  source $HOME/.rvm/scripts/rvm; bundle exec rake environment resque:work
---
+++ etc/server.js
@@ -0,0 +1,10 @@
+var redis = require("redis");
+var client = redis.createClient();
+
+var io = require("socket.io").listen(parseInt(process.env.PORT));
+
+client.on("message", function (channel, message) {
+    io.sockets.emit(channel, message);
+});
+
+client.subscribe("print");
