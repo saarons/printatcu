48d51b4a7c451f7d81703ef8e072ef8e577ff30e
Sam Aarons <samaarons@gmail.com>
2012-04-09 22:11:57 -0400

    Tweak

--- etc/server.js
+++ etc/server.js
@@ -1,10 +1,7 @@
 var redis = require("redis");
 var client = redis.createClient();
 
-var server = http.createServer();
-server.listen(parseInt(process.env.PORT), "66.228.56.230");
-
-var io = require("socket.io").listen(server);
+var io = require("socket.io").listen(parseInt(process.env.PORT));
 
 client.on("message", function (channel, message) {
     io.sockets.emit(channel, message);
