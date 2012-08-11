ee341c31e2497cd3fb21d66fe9dc40949710a767
Sam Aarons <samaarons@gmail.com>
2012-08-10 19:07:27 -0700

    Tweak bootstrap

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "pusher"
 gem "airbrake"
 gem "capistrano"
 gem "jquery-rails"
-gem "rails", "3.2.2"
+gem "rails", "3.2.8"
 gem "rvm-capistrano"
 gem "foreman", "0.39.0", :require => false
 gem "resque", :require => ["resque", "resque/server"]
@@ -23,4 +23,5 @@ group :assets do
   gem "uglifier", ">= 1.0.3"
   gem "sass-rails", "~> 3.2.3"
   gem "coffee-rails", "~> 3.2.1"
+  gem "bootstrap-sass", "~> 2.0.4.0"
 end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require_tree .
 
 var _gaq = _gaq || [];
@@ -16,4 +17,16 @@ _gaq.push(['_trackPageview']);
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '5025b7d9613f5d7b4700000b');
+  t.src = '//secure.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
 })();
