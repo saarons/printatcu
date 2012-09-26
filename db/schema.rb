44d4129cd7900f2563d991aecc013e08e6e006f0
Sam Aarons <samaarons@gmail.com>
2012-09-26 03:01:59 -0400

    Forgot schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307225523) do
+ActiveRecord::Schema.define(:version => 20120926065629) do
 
   create_table "documents", :force => true do |t|
     t.integer  "print_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20120307225523) do
     t.datetime "updated_at"
     t.boolean  "collate"
     t.integer  "copies"
+    t.string   "ip"
   end
 
 end
