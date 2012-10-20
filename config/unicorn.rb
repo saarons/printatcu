73811f95373857b6d5ca07e7997f08a66263fb09
Sam Aarons <samaarons@gmail.com>
2012-10-20 02:44:13 -0400

    add debugging

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,3 +2,5 @@ worker_processes 2
 pid "tmp/pids/unicorn.pid"
 listen "/tmp/printatcu.sock"
 working_directory ENV["UNICORN_PWD"]
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
