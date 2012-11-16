043d4ef57bac9ff5385fd825dc15c9ddd2bd6efb
Sam Aarons <samaarons@gmail.com>
2012-11-15 23:41:46 -0500

    Refactor

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,46 +1,23 @@
 class PrintsController < ApplicationController
   before_filter :cache_new, :only => :new
+  before_filter :sanitize_input, :only => :create
   
   def new
     @print = Print.new
   end
   
-  def create
-    params[:print] ||= {}
-    documents = params[:print].delete(:documents) || []
-    params[:print][:copies] = 1 if params[:print][:copies].blank?
-    
+  def create    
     @print = Print.new(params[:print])
-    @print.user = $redis.rpoplpush("users", "users") || "Malkovich"
+    @print.user = get_user
+    @print.ip = get_ip
 
-    if Rails.env.production?
-      @print.ip = request.headers["X-Real-IP"]
-    end
-    
-    documents.reject(&:blank?).each do |document|
-      doc = @print.documents.build
-      
-      case document
-      when String
-        doc.url = document
-        doc.filename = document
-      else
-        doc.filename = document.try(:original_filename)
-        doc.tempfile = document.try(:tempfile).try(:path)
-      end
-    end
+    build_documents(@print, params[:documents])
 
     respond_to do |format|
       if success = @print.save
-        @print.document_ids.each do |id|
-          Resque.enqueue(PrintWorker, id)
-        end
-        
-        flash[:user] = @print.user
-        flash[:count] = documents.size
-        flash[:printer] = @print.printer
-        flash[:building] = @print.building
-        
+        @print.enqueue
+
+        set_flash(@print)
         set_cookies(@print)
         
         format.html { redirect_to root_path(success: success) }
@@ -51,6 +28,45 @@ class PrintsController < ApplicationController
   end
   
   private
+  def sanitize_input
+    params[:print] ||= {}
+    params[:print][:documents] ||= []
+    params[:print][:documents].reject!(&:blank?)
+    params[:print][:documents].reject! { |d| d == "[]" }
+    params[:documents] = params[:print].delete(:documents)
+    params[:print][:copies] = 1 if params[:print][:copies].blank?
+  end
+
+  def get_user
+    $redis.rpoplpush("users", "users") || "Malkovich"
+  end
+
+  def get_ip
+    request.headers["X-Real-IP"] if Rails.env.production?
+  end
+
+  def build_documents(print, documents)
+    documents.each do |document|
+      doc = print.documents.build
+      
+      case document
+      when String
+        doc.url = document
+        doc.filename = document
+      else
+        doc.filename = document.try(:original_filename)
+        doc.tempfile = document.try(:tempfile).try(:path)
+      end
+    end
+  end
+
+  def set_flash(print)
+    flash[:user]     = print.user
+    flash[:printer]  = print.printer
+    flash[:building] = print.building
+    flash[:count]    = print.documents.size
+  end
+
   def set_cookies(print)
     secure = Rails.env.production?
     domain = if Rails.env.production?
--- app/models/print.rb
+++ app/models/print.rb
@@ -7,6 +7,12 @@ class Print < ActiveRecord::Base
   validates_presence_of :building, :printer, :user, :copies
   validates_inclusion_of :double_sided, :collate, :in => [true, false]
   validates_numericality_of :copies, :greater_than_or_equal_to => 1, :less_than => 100, :only_integer => true
+
+  def enqueue
+    document_ids.map do |document_id|
+      Resque.enqueue(PrintWorker, document_id)
+    end
+  end
   
   private
   def validate_documents
