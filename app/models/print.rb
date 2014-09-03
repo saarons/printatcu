d47cc2eaa74b327c4429aa2b15adfda6464fb0da
James Wen <jrw2175@columbia.edu>
2014-09-02 23:28:36 -0400

    Removed name requirement for printing

--- app/models/print.rb
+++ app/models/print.rb
@@ -3,7 +3,7 @@ class Print < ActiveRecord::Base
   
   has_many :documents
 
-  validates_presence_of :building, :printer, :user, :copies
+  validates_presence_of :building, :printer, :copies
   validates_inclusion_of :double_sided, :collate, :in => [true, false]
   validates_numericality_of :copies, :greater_than_or_equal_to => 1, :less_than => 100, :only_integer => true
 
