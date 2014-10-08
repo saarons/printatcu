a98ce94a20fbc01963b0cb6c0d0ee76adbd32248
James Wen <jrw2175@columbia.edu>
2014-10-08 02:13:06 -0400

    Resque active record fix

--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -1,8 +1,19 @@
 class PrintWorker
   @queue = :printing
+    t.string   "building"
+    t.string   "printer"
+    t.string   "user"
+    t.boolean  "double_sided", :default => true
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "collate"
+    t.integer  "copies"
+    t.string   "ip"
+  def self.perform(print_id, filename, url, building, printer, user, double_sided, collate, copies, ip)
 
-  def self.perform(document_id)
-    document = Document.find(document_id)
+    document = Document.new(print_id: print_id, filename: filename, url: url)
+    print = Print.new(building: building, printer: printer, user: user, double_sided: double_sided, collate: collate, copies: copies, ip: ip)
+    document.print = print
     document.fetch
     
     ok = document.needs_conversion? ? document.convert : true
