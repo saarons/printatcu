4b6da4f8c2d9c8cc7e347f9d0f14335075bb3572
James Wen <jrw2175@columbia.edu>
2014-10-08 02:13:49 -0400

    More fixes

--- app/workers/print_worker.rb
+++ app/workers/print_worker.rb
@@ -1,14 +1,5 @@
 class PrintWorker
   @queue = :printing
-    t.string   "building"
-    t.string   "printer"
-    t.string   "user"
-    t.boolean  "double_sided", :default => true
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
-    t.boolean  "collate"
-    t.integer  "copies"
-    t.string   "ip"
   def self.perform(print_id, filename, url, building, printer, user, double_sided, collate, copies, ip)
 
     document = Document.new(print_id: print_id, filename: filename, url: url)
