c713068ddb34eecfd87f60930f53f43cac4ceeaa
James Wen <jrw2175@columbia.edu>
2014-11-16 16:21:25 -0500

    Added precompilation of .png files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Printatcu::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.compile += %w[*.png]
 
   # Generate digests for assets URLs
   config.assets.digest = true
